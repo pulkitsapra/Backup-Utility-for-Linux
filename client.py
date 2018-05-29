40b3a308646f6647b372ff3a44e78bf2d45111c5
pulkit <pulkitsapra97@gmail.com>
2018-05-29 13:32:36 +0530

    Minute changes in sending client id , Readme

--- client.py
+++ client.py
@@ -3,11 +3,7 @@ import os
 import subprocess
 from optparse import OptionParser
 from random import randint 
-# parser = OptionParser()
-# parser.add_option("-x", dest="ip")
-# options, parser = parser.parse_args()
-# host_ip = str(options.ip)
-#host_ip='192.168.43.123'
+
 host_ip='192.168.43.123'
 port =10106
 def random_with_N_digits(n):
@@ -16,25 +12,14 @@ def random_with_N_digits(n):
     return randint(range_start, range_end)
 
 id=random_with_N_digits(4)
+id="cl"+ str(id)
 
-#def find(name, path):
-#    for root, dirs, files in os.walk(path):
-#        if name in files:
-#            return os.path.join(root, name)
-#if find('temp.pub','.'):
-#    echo "already has public key"
-#else:
-#    print "start"
-#    subprocess.call("keygen.sh")
-#    print "end"
+
 
 subprocess.call(["bash","keygen.sh"])
 
 
 s = socket.socket()
-
-
-# host_ip = socket.gethostbyname('www.google.com')
 try:
 	s.connect((host_ip,port))
 	print "the socket has successfully connected to Backup Server IP == %s" %(host_ip)
@@ -42,7 +27,7 @@ try:
 except socket.error as err:
 	print err
 
-f = open('/Users/pulkit/Desktop/sysproj/temp.pub','r')
+f = open('temp.pub','r')
 print "Sending Key to Server"
 
 j = "-";
@@ -53,26 +38,17 @@ while(l):
 	s.send(l)
 	l = f.read(8192)
 
-# try:
-# 	s.send(message)
-# except socket.error as err:
-# 	print err
-
-
-# data=s.recv(1024)
 try:
     client_id=s.recv(1024)
-    data=s.recv(10)
+    data=s.recv(12)
     ip=s.recv(24)
     print  client_id,
     print  data, ip
 except:
     print "An Unknown Error Occurred!"
 
-# print  client_id,
-
 f.close()
-with open('/Users/pulkit/Desktop/sysproj/backup_dir.txt','w') as the_file:
+with open('backup_dir.txt','w') as the_file:
     the_file.write(client_id)
     the_file.write('\n')
     the_file.write(data)
--- show.sh
+++ show.sh
@@ -1,1 +1,11 @@
-ssh sans10@192.168.43.133 'cd $1 && ls -R'
+
+IFS=$'\n' read -d '' -r -a lines < backup_dir.txt
+client_id=${lines[0]}
+dest=${lines[1]}
+ip=${lines[2]}
+echo $client_id
+echo $ip
+echo $dest
+
+
+ssh $client_id@$ip 'cd $1 && ls -R'
